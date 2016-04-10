b7294e72686220ddd03d0d707b354e5787575e5d
cliche818 <jeff.chun.yinli@gmail.com>
2016-04-10 16:47:22 -0400

    finished onBirth function for cell

--- app/components/cell.jsx
+++ app/components/cell.jsx
@@ -17,9 +17,18 @@ var Cell = React.createClass({
   },
 
   onBirth(message) {
-    if (this.props.y === message.y && this.props.x - message.x === 1) {
-      this.setState({neighbourCount: 1});
+    if (this.isNeighbour(message)) {
+      this.setState({neighbourCount: this.state.neighbourCount + 1});
     }
+  },
+
+  isNeighbour(message) {
+    return (
+      (this.props.y === message.y && Math.abs(this.props.x - message.x) === 1) ||
+      (this.props.x === message.x && Math.abs(this.props.y - message.y) === 1) ||
+      (Math.abs(this.props.x - message.x) === 1 && Math.abs(this.props.y - message.y) === 1)
+    );
+
   },
 
   render() {
--- test/cell.spec.js
+++ test/cell.spec.js
@@ -4,17 +4,44 @@ import {expect} from 'chai';
 
 import Cell from '../app/components/cell';
 
-describe('<Cell />', function () {
-  //before(function() {
-  //  this.wrapper = mount(<Cell x={1} y={1} />);
-  //});
+describe('<Cell />', function() {
+  beforeEach(() => {
+    this.wrapper = mount(<Cell x={1} y={1} />);
+    this.instance = this.wrapper.instance();
+  });
 
   describe('onBirth', () => {
-    it('should increment neighbour count if its neighbour', () => {
-      const wrapper = mount(<Cell x={1} y={1} />);
-      wrapper.instance().onBirth({x: 0, y: 1});
-      
-      expect(wrapper.instance().state.neighbourCount).to.equal(1);
+    it('should increment neighbour count if the message is from neighbours horizontally', () => {
+      this.instance.onBirth({x: 0, y: 1});
+      this.instance.onBirth({x: 2, y: 1});
+
+      expect(this.instance.state.neighbourCount).to.equal(2);
+    });
+
+    it('should increment neighbour count if the message is from neighbours vertically', () => {
+      this.instance.onBirth({x: 1, y: 0});
+      this.instance.onBirth({x: 1, y: 2});
+
+      expect(this.instance.state.neighbourCount).to.equal(2);
+    });
+
+    it('should increment neighbour count if the message is from neighbours diagonally', () => {
+      this.instance.onBirth({x: 0, y: 2});
+      this.instance.onBirth({x: 2, y: 2});
+      this.instance.onBirth({x: 0, y: 0});
+      this.instance.onBirth({x: 2, y: 0});
+
+      expect(this.instance.state.neighbourCount).to.equal(4);
+    });
+
+    it('should not increment neighbour count if its not from a neighbour', () => {
+      this.instance.onBirth({x: 3, y: 4});
+      this.instance.onBirth({x: 1, y: 4});
+      this.instance.onBirth({x: 4, y: 1});
+      this.instance.onBirth({x: 0, y: 3});
+      this.instance.onBirth({x: 3, y: 0});
+
+      expect(this.instance.state.neighbourCount).to.equal(0);
     });
   })
 
